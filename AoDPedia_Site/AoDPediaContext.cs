69929aa34fcc5c44cbbd0873aef53727655fdb76
Jack Alexander Murray <jack.murray50@gmail.com>
2022-05-11 00:20:13 -0400

    Hid connection string in appsettings

--- AoDPedia_Site/AoDPediaContext.cs
+++ AoDPedia_Site/AoDPediaContext.cs
@@ -46,8 +46,6 @@ namespace AoDPedia_Site
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=(localdb)\\ProjectsV13;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Initial Catalog=AoDPedia; ");
             }
         }
 
