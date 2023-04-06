d91206f58de4effc76e70a758a6a8354c7ac832c
Jack Alexander Murray <jack.murray50@gmail.com>
2023-04-06 19:39:44 -0400

    Improved eventual database fragmentation
    GUID isn't an appropriate clustering index. Created ROW_ID to be that instead, and keeping Id as the primary key. Going to do this to everything for likely unneeded future-proofing.

--- AoDPedia/Tables/T_Source.sql
+++ AoDPedia/Tables/T_Source.sql
@@ -1,6 +1,13 @@
 ﻿CREATE TABLE [dbo].[T_Source]
 (
-	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+	[Id] UNIQUEIDENTIFIER NOT NULL,
+    [Row_Id] INT IDENTITY(1,1) NOT NULL,
     [Name] NVARCHAR(MAX) NULL, 
-    [Abbreviation] NCHAR(10) NULL
+    [Abbreviation] NCHAR(10) NULL, 
+    CONSTRAINT [PK_T_Source_Id] PRIMARY KEY NONCLUSTERED ([Id])
+    
 )
+
+GO
+
+CREATE UNIQUE INDEX [CIX_T_Source_Column] ON [dbo].[T_Source] ([ROW_ID])
